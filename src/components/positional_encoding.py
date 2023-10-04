0109c820f5d9a2badc73a2db0d785161748b6e64
Tracy Miao <tracymiao98@gmail.com>
2023-10-03 22:07:14 -0400

    positional encoding (gaussian version)

--- src/components/positional_encoding.py
+++ src/components/positional_encoding.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from jaxtyping import Float
 from torch import Tensor
@@ -6,7 +7,7 @@ from torch import Tensor
 class PositionalEncoding(nn.Module):
     def __init__(self, num_octaves: int):
         super().__init__()
-        raise NotImplementedError("This is your homework.")
+        self.num_octaves = num_octaves
 
     def forward(
         self,
@@ -17,8 +18,14 @@ class PositionalEncoding(nn.Module):
         double the previous frequency. For each frequency, you should encode the input
         signal using both sine and cosine.
         """
+        # testing a Gaussian Fourier feature mappings with scale 10
+        # from Tancik's paper on Fourier Features
+        torch.manual_seed(0)
+        B_gauss = torch.randn((self.num_octaves * 2, samples.shape[-1])) * 10
+        x_proj = (2.0 * torch.pi * samples) @ B_gauss.T
+        embedding = torch.cat([torch.sin(x_proj), torch.cos(x_proj)], axis=-1)
 
-        raise NotImplementedError("This is your homework.")
+        return embedding
 
     def d_out(self, dimensionality: int):
-        raise NotImplementedError("This is your homework.")
+        return self.num_octaves * 2
