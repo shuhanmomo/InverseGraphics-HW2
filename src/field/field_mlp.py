8618c149837ae1f43b2d3e1eb8414fcd86f32f32
Tracy Miao <tracymiao98@gmail.com>
2023-10-03 22:44:43 -0400

    MLP building block

--- src/field/field_mlp.py
+++ src/field/field_mlp.py
@@ -1,6 +1,9 @@
+import torch.nn as nn
 from jaxtyping import Float
 from omegaconf import DictConfig
 from torch import Tensor
+
+from src.components.positional_encoding import PositionalEncoding
 
 from .field import Field
 
@@ -24,12 +27,36 @@ class FieldMLP(Field):
         """
 
         super().__init__(cfg, d_coordinate, d_out)
-        raise NotImplementedError("This is your homework.")
+        layers = []
+        input_dim = d_coordinate
+
+        # positional encoding
+        if cfg.positional_encoding_octaves is not None:
+            self.positional_encoding = PositionalEncoding(
+                cfg.positional_encoding_octaves
+            )
+            input_dim *= cfg.positional_encoding_octaves * 2
+        else:
+            self.positional_encoding = None
+
+        # Create hidden layers
+        for _ in range(cfg.num_hidden_layers):
+            layers.append(nn.Linear(input_dim, cfg.d_hidden))
+            layers.append(nn.ReLU())
+            input_dim = cfg.d_hidden
+
+        # Output layer
+        layers.append(nn.Linear(input_dim, d_out))
+
+        self.mlp = nn.Sequential(*layers)
 
     def forward(
         self,
         coordinates: Float[Tensor, "batch coordinate_dim"],
     ) -> Float[Tensor, "batch output_dim"]:
         """Evaluate the MLP at the specified coordinates."""
-
-        raise NotImplementedError("This is your homework.")
+        inputs = coordinates
+        if self.positional_encoding:
+            inputs = self.positional_encoding(coordinates)
+            # Reshape or flatten the positional encoding dimensions
+        return self.mlp(inputs)
